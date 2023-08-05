836f9b31c3eb00d2ce621b029bafb3521b17a77c
runyud <runyud@umich.edu>
2023-08-05 17:20:12 -0400

    finished adding comment model, form and some styling

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,13 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
+
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
     def __str__(self):
         return self.caption
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -20,12 +22,22 @@ class Author(models.Model):
     def __str__(self):
         return self.full_name()
 
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)  # Your Name
+    user_email = models.EmailField()  # Your Email
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,91 @@
+from typing import Any, Dict
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
+from django.views import View
+
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    # sorted_posts = sorted(all_posts, key=get_date)
-    # latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {
-        "posts" : latest_posts
-    })
 
-def posts(request):
-    all_posts =  Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-def post_detail(request, slug):
-    # identified_post = next(post for post in all_posts if post['slug'] == slug)
-    # identified_post = Post.objects.get(slug=slug)
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+#     # sorted_posts = sorted(all_posts, key=get_date)
+#     # latest_posts = sorted_posts[-3:]
+#     return render(request, "blog/index.html", {
+#         "posts" : latest_posts
+#     })
+
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+# def posts(request):
+#     all_posts =  Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html", {
+#         "all_posts": all_posts
+#     })
+
+
+class SinglePostView(View):
+    # template_name = "blog/post-detail.html"
+    # model = Post
+
+    def get(self, request, slug):
+        identified_post = get_object_or_404(Post, slug=slug)
+        context = {
+            "post": identified_post,
+            "post_tags": identified_post.tags.all(),
+            "comment_form" : CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        identified_post = get_object_or_404(Post, slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = identified_post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": identified_post,
+            "post_tags": identified_post.tags.all(),
+            "comment_form" : comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["post_tags"] = self.get_object().tags.all()
+    #     context["comment_form"] = CommentForm()
+    #     return context
+ 
+# def post_detail(request, slug):
+#     # identified_post = next(post for post in all_posts if post['slug'] == slug)
+#     # identified_post = Post.objects.get(slug=slug)
+#     identified_post = get_object_or_404(Post, slug=slug)
+#     return render(request, "blog/post-detail.html", {
+#         "post": identified_post,
+#         "post_tags": identified_post.tags.all()
+#     })
